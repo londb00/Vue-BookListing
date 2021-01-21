c9b6cde1d19e68c7279d1f07a3bf7f07fb2767bb
Bas London <londonb@gmail.com>
2021-01-21 12:58:29 +0100

    completed

--- src/components/BookItem.vue
+++ src/components/BookItem.vue
@@ -1,14 +1,24 @@
 <template>
-  <li v-for="book in books">{{book.title}}:{{book.author}}</li>
+  <li>{{book.title}}:{{book.author}}</li>
 </template>
 
 <script>
 export default {
-name: "BookItem",
-props:"book"
+  name: "BookItem",
+  props: ["book"]
 }
 </script>
 
-<style scoped>
-
+<style>
+h1, h2 {
+    font-weight: normal
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    display: block;
+    margin: 0 10px;
+}
 </style>
